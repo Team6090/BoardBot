853a14087ce8ca819d28be816bc5531db5f39e3f
Zak <zakevans1@outlook.com>
2023-03-01 20:18:32 -0500

    Limit Switch

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,10 +6,15 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.subsystems.LimitSwitch;
 
 public class RobotContainer {
+
+  private LimitSwitch limitSwitch = new LimitSwitch();
+
   public RobotContainer() {
     configureBindings();
+   
   }
 
   private void configureBindings() {}
